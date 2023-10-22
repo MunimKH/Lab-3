b26c47abe07d8743b3cec971541aeee484b6c4ad
MunimKH <munimasim1213@gmail.com>
2023-10-22 21:01:24 +0500

    War Gaya

--- feature1.py
+++ feature1.py
@@ -71,3 +71,90 @@ class Library:
         else:
             print("Sorry !!! But there is no mentioned book !!! ")
             return
+    def searchbook(self):
+        self.searchbook=input("Enter the Book Name/Author Name/Genre !!! ")
+        for i in self.bookdata:
+            if self.searchbook in i:
+                print(f"Book : {i[0]} ..... Author : {i[1]} ...... Genre : {i[3]}")
+        return "Sorry !!! We can not find related Book !"
+    def checkin(self):
+        self.nameofcbook=input("Which book did you checked out !!!")
+        userdata=pd.read_csv("data.csv")
+        userlistdata=userdata.values.tolist()
+        for i in userlistdata:
+            if i[4] == True:
+                for j in self.bookuser:
+                    if i[0] in j and self.nameofcbook in j:
+                        print("Sure !!! Thanks for returning the book ! Hope you enjoyed reading it ! ('-')")
+                        self.bookuser.remove(j)
+                        return
+                else: 
+                    print("Sorry !!! That does not match our records !!! ")
+    def userRecord(self):
+        print("Sure ! Here You Go !!! ")
+        userdata=pd.read_csv("data.csv")
+        userlistdata=userdata.values.tolist()
+        for m in userlistdata:
+            if m[4]==True:
+                for j in self.bookuser:
+                    if m[0] in j:
+                        print(f"name : {j[0]} .... book : {j[1]}")
+    def Bookall(self):
+        print("Here You go !!! ")
+        for i in self.bookdata:
+            print(f"Book : {i[0]} ..... Author : {i[1]} ...... Genre : {i[3]}")
+    def singout(self):
+        print("Sure ! Here You Go !!! ")
+        print("You Are Signed Out !!!")
+        for m in self.datalist:
+            if m[4]==True:
+                data.loc[data['name'] == m[0], 'login'] = False
+                
+library = Library()
+signed_in = False
+
+while True:
+    print("\n=========================")
+    print("Welcome to Library System")
+    print("=========================")
+    if not signed_in:
+        print("1. Sign Up")
+        print("2. Log In")
+    else:
+        print(f"Welcome to LMA System !")
+        print("3. Issue Book")
+        print("4. Search Book")
+        print("5. Check In")
+        print("6. User Record")
+        print("7. View All Books")
+        print("8. Sign Out")
+
+    print("0. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1' and not signed_in:
+        library.Signup()
+    elif choice == '2' and not signed_in:
+        library.Login()
+        signed_in = True
+    elif choice == '3' and signed_in:
+        library.issuebook()
+    elif choice == '4' and signed_in:
+        library.searchbook()
+    elif choice == '5' and signed_in:
+        library.checkin()
+    elif choice == '6' and signed_in:
+        library.userRecord()
+    elif choice == '7' and signed_in:
+        library.Bookall()
+    elif choice == '8' and signed_in:
+        library.singout()
+        signed_in = False
+    elif choice == '0':
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
+        
